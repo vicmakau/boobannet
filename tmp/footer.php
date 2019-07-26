edf22f678fd331ac67520419c97bf3213b9d184b
victor m <41968620+victorKM@users.noreply.github.com>
2019-07-26 10:35:33 +0300

    Add files via upload

---
+++ tmp/footer.php
@@ -0,0 +1,139 @@
+<footer class="footer-distributed col-lg-12">
+
+
+	<div class="footer-left">
+
+
+		<h3>Booban<span>Team</span></h3>
+
+		<p class="footer-links">
+			<a href="#">Home</a>
+			·
+			<a href="#">Our team</a>
+			·
+			<a href="#">Pricing</a>
+			·
+			<a href="#">About</a>
+			·
+			<a href="#">Faq</a>
+			·
+			<a href="#">Contact</a>
+		</p>
+
+		<p class="footer-company-name">Booban Team &copy; 2019</p>
+
+
+	</div>
+
+
+
+	<div class="footer-center">
+
+		<div>
+
+			<i class="fa fa-envelope"></i>
+
+			<p><a href="mailto:support@company.com">support@booban.com</a></p>
+
+		</div>
+
+	</div>
+
+
+
+	<div class="footer-right">
+
+		<p class="footer-company-about">
+
+			<span>Connect with us</span>
+
+		</p>
+
+		<div class="footer-icons">
+
+			<a href="#"><i class="fa fa-facebook"></i></a>
+
+			<a href="#"><i class="fa fa-twitter"></i></a>
+
+			<a href="#"><i class="fa fa-linkedin"></i></a>
+
+			<a href="#"><i class="fa fa-github"></i></a>
+
+		</div>
+
+	</div>
+
+
+</footer>
+
+	
+
+<script>
+
+	$('#posters').carousel({
+
+
+		interval: 5000
+
+
+		});
+
+
+
+
+
+	new WOW().init();
+
+
+	
+
+</script>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+<!-- member --> 
+
+
+
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+
+    <script src="assets/js/popper.min.js"></script>
+
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <script src="assets/js/parallax.min.js"></script>
+
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+
+
+    <script src="assets/js/jquery.appear.min.js"></script>
+
+    <script src="assets/js/scrolling-nav.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+
+
+    <script src="assets/js/main.js"></script>
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+
+
+
+
+
+</body>
+
+
+
---
+++ tmp/header.php
@@ -0,0 +1,119 @@
+<!-- Boobannet 
+By Victor KM
+github.com/victorKM 
+see readme.md
+-->
+
+
+<?php
+
+  session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <title>Booban | Tech spot</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width = device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.min.css">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+
+
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+
+   
+
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="assets/css/default.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+
+
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+
+    <link rel="stylesheet" href="../assets/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+
+
+    <link href='https://fonts.googleapis.com/css?family=PT Sans' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+
+    
+</head>
+
+
+<body>
+  
+  <nav class="navbar navbar-default navigation-clean-button">
+
+
+      <div class="container-fluid">
+        
+          <div class="navbar-header"><a class="navbar-brand" href="index.php">Booban Team</a>
+              <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+          </div>
+
+
+          <div class="collapse navbar-collapse" id="navcol-1">
+
+              <ul class="nav navbar-nav">
+
+
+                  
+                  <li role="presentation"><a href="#">BLOG</a></li>
+                  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">HIRE US <span class="caret"></span></a>
+
+
+                      <ul class="dropdown-menu" role="menu">
+                          <li role="presentation"><a href="#">Grahics design</a></li>
+                          <li role="presentation"><a href="#">App Development</a></li>
+                          <li role="presentation"><a href="#">Video & Photography</a></li>
+                      </ul>
+
+
+                  </li>
+
+
+              </ul>
+
+              <p class="navbar-text navbar-right actions"><a class="navbar-link login" href="login.php">Log In</a> <a class="btn btn-default action-button" role="button" href="signup.php">Sign Up</a></p>
+
+
+          </div>
+
+
+      </div>
+
+
+  </nav>
